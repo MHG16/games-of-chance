33b00f4df841601fcfbf47fe941a736e3fa7f6a0
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-16 15:47:16 -0500

    Working on getProbabilities

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 function playingCard() {
 	this.suit = ['spades', 'hearts', 'clubs', 'diamonds'];
-	this.value = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'King', 'Queen'];
+	this.value = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'King', 'Queen'];
 }
 
-function sixSidedDie {
+function sixSidedDie() {
 	this.number = [1, 2, 3, 4, 5, 6];
 	this.roll = function () {
 
@@ -14,9 +14,38 @@ function sixSidedDie {
 	}	
 }
 
-function getProbabilities {
 
-	
+
+
+//Inputs: None.  
+//Outputs: How many occurrences for each possible combination: 2, 3, 4,...12.   
+function getProbabilities(die1, die2) {
+
+	this.sum = function () {
+
+		var occurrences = [];
+		var numRolls = 1000;
+		var total = 0;
+
+		for (var i = 0; i < numRolls; i++) {
+
+			die1 = sixSidedDie();
+			die2 = sixSidedDie(); 
+
+			total = die1 + die2;
+
+			if (total in occurrences) {
+			//Update it's number on the list one by one
+			occurrences[total]++;
+			}
+			//Otherwise
+			else {
+			//Add it to the list with a count of one
+				occurrences[total] = 1;
+			}
+		}
+
+	}
 }
 
 
